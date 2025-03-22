9c3ade1f090233bdc222b1d968c9e5a9c3cb29e8
syur <bp113534@email.com>
2025-03-22 15:58:01 +0100

    nuevo componente

---
+++ FronEnd-ANGULAR/src/app/admin/admin-backoffice/admin-backoffice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-backoffice',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-backoffice.component.html',
+  styleUrl: './admin-backoffice.component.scss'
+})
+export class AdminBackofficeComponent {
+
+}
